2fac37b6463fe181bac262528d587bc90a89489e
Varun Srinivasan <varunsrin@gmail.com>
2015-01-25 14:52:20 -0800

    cleaning up function calls and return values

--- minpay.rb
+++ minpay.rb
@@ -7,23 +7,20 @@ def file_path
 end
 
 def transactions
-  transactions = []
   CSV.foreach(file_path) do |row|
     debtor = row[0]
     creditors = row.drop(2)
     amount = row[1].to_i / creditors.length
     creditors.each do |creditor|
-      transactions << Transaction.new(debtor, creditor, amount)
+      yield Transaction.new(debtor, creditor, amount)
     end
   end
-  transactions
+end
+
+def print_transaction(t)
+  puts t.creditor.to_s + ' owes ' + t.debtor.to_s + ' $' + t.amount.to_s
 end
 
 accounts = Ledgerous.new
-transactions.each do |t|
-  accounts.reconcile(t)
-end
-
-accounts.settle.each do |t|
-  puts t.creditor.to_s + ' owes ' + t.debtor.to_s + ' $' + t.amount.to_s
-end
+transactions { |t| accounts.reconcile(t) }
+accounts.settle.each { |t| print_transaction(t) }
